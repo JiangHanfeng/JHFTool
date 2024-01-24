b7a839dd8ff55e1253c64bd7d0d7bf6eb38b243f
蒋函锋 <wiwi@jianghanfengdeMac-mini.local>
2024-01-24 16:28:09 +0800

    0.1.8

---
+++ JHFTool/Classes/JHFCancelableTask.swift
@@ -0,0 +1,34 @@
+//
+//  JHFCancelableTask.swift
+//  JHFTool
+//
+//  Created by 蒋函锋 on 2024/1/24.
+//
+
+import Foundation
+
+public class JHFCancelableTask : NSObject {
+    typealias Excutable = (_ isCanceled: Bool) -> ()
+    
+    @discardableResult static func delay(_ time: TimeInterval, at queue: dispatch_queue_t, task: @escaping () -> ()) -> Excutable? {
+        var closure: (() -> Void)? = task
+        var result: Excutable?
+        let delayClosure: Excutable = { isCancelled in
+            defer {
+                closure = nil
+                result = nil
+            }
+            guard let closure, !isCancelled else {
+                return
+            }
+            queue.async(execute: closure)
+        }
+        result = delayClosure
+        queue.asyncAfter(deadline: .now() + time) {
+            if let result {
+                result(false)
+            }
+        }
+        return result
+    }
+}
